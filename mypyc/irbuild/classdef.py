1a7f2930d67610b32c017268a31c5e5697b3b16e
Chad Dombrova <chadrik@gmail.com>
2025-05-06 21:08:46 -0700

    Fix error where __eq__ declaration exists but the method does not

--- mypyc/irbuild/classdef.py
+++ mypyc/irbuild/classdef.py
@@ -792,7 +792,7 @@ def check_deletable_declaration(builder: IRBuilder, cl: ClassIR, line: int) -> N
 def create_ne_from_eq(builder: IRBuilder, cdef: ClassDef) -> None:
     """Create a "__ne__" method from a "__eq__" method (if only latter exists)."""
     cls = builder.mapper.type_to_ir[cdef.info]
-    if cls.has_method("__eq__") and not cls.has_method("__ne__"):
+    if cls.get_method("__eq__") and not cls.has_method("__ne__"):
         gen_glue_ne_method(builder, cls, cdef.line)
 
 
